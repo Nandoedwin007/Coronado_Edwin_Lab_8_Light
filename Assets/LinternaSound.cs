d038c111a07695b77a1ec60b47c6850507f4d8b0
Edwin Coronado <cor14148@uvg.edu.gt>
2019-05-15 11:38:34 -0600

    Version Casi Final
    Falta comentarios y revision completa de funcionamiento

--- Assets/LinternaSound.cs
+++ Assets/LinternaSound.cs
@@ -7,6 +7,7 @@ public class LinternaSound : MonoBehaviour
 
     public AudioClip SoundToPlayClick;
     private AudioSource audioSrc;
+    public Light luzHabitacion;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,31 @@ public class LinternaSound : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             audioSrc.PlayOneShot(SoundToPlayClick, 1f);
+        }
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+ 
+        if (other.gameObject.tag == "Habitacion2")
+        {
+            luzHabitacion.enabled = true;
+            Debug.Log("Se ha entrado a la habitacion");
+        }
+        else
+        {
+            luzHabitacion.enabled = false;
+            Debug.Log("Se ha salido a la habitacion");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Habitacion2")
+        {
+            luzHabitacion.enabled = false;
+            Debug.Log("Se ha salido a la habitacion");
         }
     }
 }
---
+++ Assets/PointLightScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointLightScript : MonoBehaviour
+{
+    private Light pointLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointLight.intensity = Mathf.Sin(Time.time*5)*5;
+    }
+}
